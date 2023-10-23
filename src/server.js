d67525f4ef8304af177a435dcebe323960d06585
Gabriel Piva <89921667+gabriel-piva@users.noreply.github.com>
2023-10-23 13:52:22 -0300

    initial structure

---
+++ src/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+
+app.use(express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname, "public"));
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "html");
+
+app.use("/", (req, res) => {
+    res.render("index.html");
+})
+
+io.on("connection", socket => {
+    console.log(`Socket Conectado: ${socket.id}`);
+});
+
+server.listen(8080);

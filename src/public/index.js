2adf12021b2dd2d00770661273e1c78c59c10d1b
Gabriel Piva <89921667+gabriel-piva@users.noreply.github.com>
2023-10-23 22:15:55 -0300

    chat box working

--- src/public/index.js
+++ src/public/index.js
@@ -1,1 +1,36 @@
 const socket = io('http://localhost:8080');
+
+const chatBox = document.querySelector('#chatBox');
+const sendMessage = () => {
+    console.log('Send')
+    const username = document.querySelector('#username').value;
+    const newMessage = document.querySelector('#new-message').value;
+
+    if(username.length && newMessage.length) {
+        let messageObj = {
+            username: username,
+            message: newMessage
+        }
+        renderMessage(messageObj);
+        socket.emit('sendMessage', messageObj);
+        document.querySelector('#new-message').value = "";
+    }
+}
+const renderMessage = (message) => {
+    chatBox.innerHTML +=`
+        <div class="message">
+            <span class="username">${message.username}</span>
+            ${message.message}
+        </div>
+    `;
+    chatBox.scrollTop = chatBox.scrollHeight
+}
+const handleKeydown = (e) => {
+    if(e.key == 'Enter') {
+        sendMessage();
+    }   
+}
+
+socket.on('receivedMessage', (message) => renderMessage(message));
+socket.on('previousMessages', (messages) => messages.forEach(message => renderMessage(message)))
+document.addEventListener('keydown', handleKeydown);
--- src/server.js
+++ src/server.js
@@ -1,20 +1,27 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const path = require("path");
-const server = require("http").createServer(app);
-const io = require("socket.io")(server);
+const path = require('path');
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 
-app.use(express.static(path.join(__dirname, "public")));
-app.set("views", path.join(__dirname, "public"));
-app.engine("html", require("ejs").renderFile);
-app.set("view engine", "html");
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'public'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
-app.use("/", (req, res) => {
-    res.render("index.html");
+app.use('/', (req, res) => {
+    res.render('index.html');
 })
 
-io.on("connection", socket => {
+const messages = [];
+io.on('connection', socket => {
     console.log(`Socket Conectado: ${socket.id}`);
+    socket.emit('previousMessages', messages);
+    socket.on('sendMessage', data => {
+        messages.push(data);
+        socket.broadcast.emit('receivedMessage', data);
+    });
+
 });
 
-server.listen(8080, () => console.log("Servidor em execução."));
+server.listen(8080, () => console.log('Servidor em execução.'));
